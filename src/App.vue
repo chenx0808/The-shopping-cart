e3be1e97e197334b33f0e3dd5fe5aa6bde20948f
chenx0808 <2264119591@qq.com>
2022-06-26 18:01:15 +0800

    完成自定义头部

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyHeade title="购物车案例" background="tomato" color="#fff"></MyHeade>
+    <MyGoods></MyGoods>
+    <MyFooter></MyFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+import MyHeade from './components/MyHeade.vue'
+import MyGoods from './components/MyGoods.vue'
+import MyFooter from './components/MyFooter.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+export default { components: { MyHeade, MyGoods, MyFooter } }
 </script>
 
-<style lang="less">
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 50px 0;
 }
 </style>
