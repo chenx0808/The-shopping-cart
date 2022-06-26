8290a7e7343def22a1be95fde5eaa267290cebc1
chenx0808 <2264119591@qq.com>
2022-06-26 20:51:14 +0800

    完成全部

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -14,10 +14,12 @@
     <!-- 合计 -->
     <div>
       <span>合计:</span>
-      <span class="price">¥ 0</span>
+      <span class="price">¥ {{ allPrice }}</span>
     </div>
     <!-- 按钮 -->
-    <button type="button" class="footer-btn btn btn-primary">结算 ( 0 )</button>
+    <button type="button" class="footer-btn btn btn-primary">
+      结算 ( {{ allCount }} )
+    </button>
   </div>
 </template>
 
@@ -37,6 +39,24 @@ export default {
       set (newVal) {
         this.goodlist.forEach((item) => (item.goods_state = newVal))
       }
+    },
+    allCount () {
+      return this.goodlist.reduce((pre, curr) => {
+        if (curr.goods_state) {
+          return pre + curr.goods_count
+        } else {
+          return pre
+        }
+      }, 0)
+    },
+    allPrice () {
+      return this.goodlist.reduce((pre, curr) => {
+        if (curr.goods_state) {
+          return pre + curr.goods_count * curr.goods_price
+        } else {
+          return pre
+        }
+      }, 0)
     }
   }
 }
